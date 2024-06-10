0bf8b0606160cc01d9f51e5721a8e11ea27b5b3b
Elena <elenarock28@gmail.com>
2024-06-10 21:06:45 +0300

    less-62 settings for H2 database into application.properties update methods into BookServiceImpl and query into repositories change database (into pom.xml) from H2 to PostgreSql by aiven.io
    hw: connect DBeawer connect to PostgreSQL by aiven.io

--- src/main/java/telran/java52/book/dao/BookRepository.java
+++ src/main/java/telran/java52/book/dao/BookRepository.java
@@ -1,20 +1,16 @@
 package telran.java52.book.dao;
 
-import java.util.List;
+import java.util.stream.Stream;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
-import telran.java52.book.model.Author;
 import telran.java52.book.model.Book;
-import telran.java52.book.model.Publisher;
 
 public interface BookRepository extends JpaRepository<Book, String> {
 
-//	@Query("select b from Book b where b.authors.contains(?1)")
-	 @Query("select b from Book b join b.authors a where a.name = ?1")
-	List<Book> findBooksByAuthor(Author author);
+	Stream<Book> findByPublisherPublisherName(String name);
 
-	 @Query("select b from Book b  where b.publisher = ?1")
-	List<Book> findBooksByPublisher(Publisher publisher);
+	Stream<Book> findByAuthorsName(String name);
+
+	void deleteByAuthorsName(String name);
 }
